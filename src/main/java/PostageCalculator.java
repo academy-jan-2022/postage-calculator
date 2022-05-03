7ce555af36c5621d813b2c15fb897dd24c2b0896
danihra-codurance <daniel.rodriguez@codurance.com>
2022-05-03 14:22:04 +0200

    refactor

--- src/main/java/PostageCalculator.java
+++ src/main/java/PostageCalculator.java
@@ -1,10 +1,18 @@
 import java.math.BigDecimal;
 
 public class PostageCalculator {
+
+    public static final BigDecimal TIMES_4 = BigDecimal.valueOf(4);
+
     public Money calculate(int weight, int height, int width, int depth, Currency currency) {
+        BigDecimal amount;
+
         if (weight > 60) {
-            return new Money(BigDecimal.valueOf(weight).multiply(BigDecimal.valueOf(4)), Currency.GBP);
+            amount = BigDecimal.valueOf(weight).multiply(TIMES_4);
+        } else {
+            amount = BigDecimal.valueOf(120);
         }
-        return new Money(BigDecimal.valueOf(120), Currency.GBP);
+
+        return new Money(amount, Currency.GBP);
     }
 }
