6446e110ff4324c299c76f073fda5f3ed0570886
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-05-03 12:20:20 +0200

    green

--- src/main/java/PostageCalculator.java
+++ src/main/java/PostageCalculator.java
@@ -2,6 +2,9 @@ import java.math.BigDecimal;
 
 public class PostageCalculator {
     public Money calculate(int weight, int height, int width, int depth, Currency currency) {
+        if(weight > 60){
+            return new Money(BigDecimal.valueOf(1000), Currency.GBP);
+        }
         return new Money(BigDecimal.valueOf(120), Currency.GBP);
     }
 }
