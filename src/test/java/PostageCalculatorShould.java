0b54df2f732c5347fa8ffa04a05d3445be8f1299
danihra-codurance <daniel.rodriguez@codurance.com>
2022-05-03 12:16:29 +0200

    red

--- src/test/java/PostageCalculatorShould.java
+++ src/test/java/PostageCalculatorShould.java
@@ -15,4 +15,15 @@ class PostageCalculatorShould {
         Assertions.assertEquals(BigDecimal.valueOf(120), actualAmount.amount());
         Assertions.assertEquals(Currency.GBP, actualAmount.currency());
     }
+
+    @Test
+    void calculate_variable_cost_dependant_on_weight() {
+        PostageCalculator postageCalculator = new PostageCalculator();
+
+        Money actualAmount = postageCalculator.calculate(250, 220, 150, 10, Currency.GBP);
+
+        Assertions.assertEquals(BigDecimal.valueOf(1000), actualAmount.amount());
+        Assertions.assertEquals(Currency.GBP, actualAmount.currency());
+
+    }
 }
