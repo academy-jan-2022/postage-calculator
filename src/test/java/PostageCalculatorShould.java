6449ad4d6acd162f46237803c3cf4d82e8f2cd3d
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-05-03 12:29:59 +0200

    refactor

--- src/test/java/PostageCalculatorShould.java
+++ src/test/java/PostageCalculatorShould.java
@@ -1,18 +1,21 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.math.BigDecimal;
 
 class PostageCalculatorShould {
 
-    @Test
-    void calculate_for_a_fixed_cost_of_120() {
+    @ParameterizedTest
+    @CsvSource({"50, 220, 150, 10, 120"})
+    void calculate_for_a_fixed_cost_of_120(int weight, int height, int width, int depth, int expected) {
 
         PostageCalculator postageCalculator = new PostageCalculator();
 
-        Money actualAmount = postageCalculator.calculate(50, 220, 150, 10, Currency.GBP);
+        Money actualAmount = postageCalculator.calculate(weight, height, width, depth, Currency.GBP);
 
-        Assertions.assertEquals(BigDecimal.valueOf(120), actualAmount.amount());
+        Assertions.assertEquals(BigDecimal.valueOf(expected), actualAmount.amount());
         Assertions.assertEquals(Currency.GBP, actualAmount.currency());
     }
 
