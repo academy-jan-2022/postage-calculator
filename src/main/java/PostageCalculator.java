931a6e37f45230334133cf509314107d9e5d782f
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-05-03 14:30:03 +0200

    refactor

--- src/main/java/PostageCalculator.java
+++ src/main/java/PostageCalculator.java
@@ -7,12 +7,16 @@ public class PostageCalculator {
     public Money calculate(int weight, int height, int width, int depth, Currency currency) {
         BigDecimal amount;
 
-        if (weight > 60 || height < 324 && height > 229 ) {
+        if (isTierTwoPrice(weight, height)) {
             amount = BigDecimal.valueOf(weight).multiply(TIMES_4);
         } else {
             amount = BigDecimal.valueOf(120);
         }
 
         return new Money(amount, Currency.GBP);
+    }
+
+    private boolean isTierTwoPrice(int weight, int height) {
+        return weight > 60 || height < 324 && height > 229;
     }
 }
